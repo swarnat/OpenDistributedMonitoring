9690a00e3982f1f8bc7178d3791d49175bbdafea
Stefan Warnat <warnat@redoo-networks.com>
2022-07-18 00:12:14 +0200

    update install script

--- controller/install.js
+++ controller/install.js
@@ -30,17 +30,17 @@ mysql.connect().then(function() {
         \`text\` varchar(60) NOT NULL,
         PRIMARY KEY (\`checkid\`,\`created\`),
         CONSTRAINT \`history_ibfk_1\` FOREIGN KEY (\`checkid\`) REFERENCES \`checks\` (\`id\`) ON DELETE CASCADE
-      )`);      
+      ) ENGINE=InnoDB`);      
 
       console.log('  - Table: apitoken');      
       
       connection.query(`CREATE TABLE IF NOT EXISTS \`apitoken\` (
-        `id` varchar(42) NOT NULL,
-        `token` varchar(66) NOT NULL,
-        `expire` datetime DEFAULT NULL,
+        \`id\` varchar(42) NOT NULL,
+        \`token\` varchar(66) NOT NULL,
+        \`expire\` datetime DEFAULT NULL,
         PRIMARY KEY (`id`),
         UNIQUE KEY `token` (`token`)
-      )`);
+      ) ENGINE=InnoDB`);
 
       // Token: OpenDistributedMonitoring
       connection.query(`INSERT IGNORE INTO`apitoken` SET id = UUID(), token = "93c17c6d5319e19c34eeff5c9c03706ad53e5c08e8adc3c15eefcc5b0eb3d874", expire = NULL`);
