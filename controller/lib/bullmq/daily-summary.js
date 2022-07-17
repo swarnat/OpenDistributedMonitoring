76b33134ac9be74d6dfd5425bd11da0b573ca55c
Stefan Warnat <warnat@redoo-networks.com>
2022-07-18 00:17:54 +0200

    update daily-summary.js

--- controller/lib/bullmq/daily-summary.js
+++ controller/lib/bullmq/daily-summary.js
@@ -8,34 +8,37 @@ import axios from 'axios';
 const dailyQueueScheduler = new QueueScheduler('daily',  { connection: configuration.redis });
 const dailyQueue = new Queue('daily',  { connection: configuration.redis });
   
-await dailyQueue.drain();
+dailyQueue.drain().then(() => {
+    
+    new Worker('daily', job => {
+        var connection = mysql.getConnection();
+    
+        try {
+            var expire = dayjs().subtract(24, 'hours')
+    
+            let until = expire.format('YYYY-MM-DD HH:mm:ss');
+    
+            connection.query('SELECT COUNT(*) as num FROM history WHERE created > ?', [until], (error, results, fields) => {
+                log('SUMMARY', 'Send daily report about ' + results[0].num + ' history records');
+    
+                axios.post(configuration.slack.webhook, {
+                  "text": "<em>[Summary]</em> With last 24 hours, there was <b>" + results[0].num + ' checks</b> done',
+                  //"text": "<b>Recover check</b> " + checkData[checkId].title + '<br/>after ' + timeRange,
+                  "format": "html",
+                  "displayName": configuration.slack.displayName,
+              });                 
+            });
+        } catch (e) {
+            console.log(e);
+        }
+        //
+    
+    
+        //
+      }, { connection: configuration.redis });
+    
+});
 
-new Worker('daily', job => {
-    var connection = mysql.getConnection();
-
-    try {
-        var expire = dayjs().subtract(24, 'hours')
-
-        let until = expire.format('YYYY-MM-DD HH:mm:ss');
-
-        connection.query('SELECT COUNT(*) as num FROM history WHERE created > ?', [until], (error, results, fields) => {
-            log('SUMMARY', 'Send daily report about ' + results[0].num + ' history records');
-
-            axios.post(configuration.slack.webhook, {
-              "text": "<em>[Summary]</em> With last 24 hours, there was <b>" + results[0].num + ' checks</b> done',
-              //"text": "<b>Recover check</b> " + checkData[checkId].title + '<br/>after ' + timeRange,
-              "format": "html",
-              "displayName": configuration.slack.displayName,
-          });                 
-        });
-    } catch (e) {
-        console.log(e);
-    }
-    //
-
-
-    //
-  }, { connection: configuration.redis });
 
 export default {
     reset: function() {
