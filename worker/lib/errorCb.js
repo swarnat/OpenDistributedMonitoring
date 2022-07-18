c55ee32774d78b3506a65a9f0c3b0cbe875b0e82
Stefan Warnat <warnat@redoo-networks.com>
2022-07-19 00:11:58 +0200

    udpate result tropic

--- worker/lib/errorCb.js
+++ worker/lib/errorCb.js
@@ -2,7 +2,7 @@ import { Queue } from 'bullmq';
 
 import configuration from '../config.js';
 
-const resultQueue = new Queue('results',  { connection: configuration.redis });
+const resultQueue = new Queue(configuration.topic_prefix + 'results',  { connection: configuration.redis });
 
 
 export default function(jobName, errorData) {
--- worker/lib/successCb.js
+++ worker/lib/successCb.js
@@ -1,7 +1,7 @@
 import { Queue } from 'bullmq';
 import configuration from '../config.js';
 
-const resultQueue = new Queue('results',  { connection: configuration.redis });
+const resultQueue = new Queue(configuration.topic_prefix + 'results',  { connection: configuration.redis });
 
 export default function(jobName, results) {
 
